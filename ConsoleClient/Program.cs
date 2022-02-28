d82cf2ea763a32409398f61784fddb10ff418ca8
Таджури Даниил <tadzhuri.diu@phystech.edu>
2022-03-01 01:48:33 +0300

    Поработали с сериализацией

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMassenger
 {
+    [Serializable]
     class Programm
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Начало проекта!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //Console.WriteLine("Начало проекта!");
+            //Console.WriteLine(msg.ToString());
         }
     }
 }
