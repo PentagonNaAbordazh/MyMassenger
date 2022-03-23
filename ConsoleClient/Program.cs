3bac447bb743e7355ebe14a3dc0541b3954b8067
Таджури Даниил <tadzhuri.diu@phystech.edu>
2022-03-23 03:13:24 +0300

    Создан клиент, обрабатывающий запросы Get и Post. Написана основная программа Program.cs отвечающая за отображение примитивного интерфеса мессенджера, определеа функция получения и отправки сообщений

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -6,15 +6,45 @@ namespace MyMassenger
     [Serializable]
     class Programm
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessangerClientAPI API = new MessangerClientAPI();
+
+        private static void GetNewMessages()
+        //this fucntion get all messages and displays them on the screen 
+        {
+            Message msg = API.GetMessage(MessageID);
+            while(msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
-            string output = JsonConvert.SerializeObject(msg);
-            Console.WriteLine(output);
-            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
-            Console.WriteLine(deserializedMsg);
+            //Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
             //Console.WriteLine("Начало проекта!");
             //Console.WriteLine(msg.ToString());
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя");
+            //UserName = "RusAl";
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
     }
 }
